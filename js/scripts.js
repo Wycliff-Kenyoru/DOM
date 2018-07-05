d046dd7ebeef6850ecf0e38dab4254cb549d7fe7
Wycliff-Kenyoru <wycliffkerongo@gmail.com>
2018-07-05 16:15:12 +0300

    color changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,18 @@ $(document).ready(function() {
         $("button#goodbye").click(function() {
           $("ul#user").prepend("<li>Goodbye!</li>");
           $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+          $('li').css('background-color', 'red');
         });
 
         $("button#stop").click(function() {
           $("ul#user").prepend("<li>Stop copying me!</li>");
           $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+          $('li').css('background-color', 'yellow');
         });
          $("button#hello").click(function() {
         $("ul#user").prepend("<li>Hello!</li>");
         $("ul#webpage").prepend("<li>Why hello there!</li>");
         $('li').css('background-color', 'green');
       });
-    
+
     });
